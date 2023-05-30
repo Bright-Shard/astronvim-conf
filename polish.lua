1502914c0213c87e934ac9944342958933bdbf65
BrightShard <brightshard@brightshard.dev>
2023-05-29 23:18:58 -0400

    Enable rust inlay hints & use cargo clippy

--- polish.lua
+++ polish.lua
@@ -3,4 +3,16 @@ return function()
   require("presence").setup({
     blacklist = require("user.blacklist")
   })
+  -- Set up Rust inlay hints
+  require("rust-tools").inlay_hints.enable()
+  -- Use 'cargo clippy' for checking Rust code
+  require("lspconfig").rust_analyzer.setup({
+    settings = {
+      ["rust-analyzer"] = {
+        check = {
+          command = "clippy"
+        }
+      }
+    }
+  })
 end
