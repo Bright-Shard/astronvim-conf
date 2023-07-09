c051cb8872dcd405268f8b436cd613208fdbf135
BrightShard <brightshard@brightshard.dev>
2023-07-08 20:36:48 -0400

    Launch in dev directory

--- polish.lua
+++ polish.lua
@@ -3,16 +3,7 @@ return function()
   require("presence").setup({
     blacklist = require("user.blacklist")
   })
-  -- Set up Rust inlay hints
-  require("rust-tools").inlay_hints.enable()
-  -- Use 'cargo clippy' for checking Rust code
-  require("lspconfig").rust_analyzer.setup({
-    settings = {
-      ["rust-analyzer"] = {
-        check = {
-          command = "clippy"
-        }
-      }
-    }
-  })
+
+  -- Move to the dev directory
+  vim.cmd("cd ~/Dev")
 end
